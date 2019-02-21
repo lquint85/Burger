f8c0edb18b06af943c26c7dac92ffebce4bab545
Lucent <lquint85@gmail.com>
2019-02-21 15:59:02 -0600

    modified controllers.js

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -1,37 +1,77 @@
+
 const express = require("express");
 const router = express.Router();
-
-
-const burgerModel = require("../models/burger");
-
+const burger = require("../models/burger");
+// console.log(burger);
 router.get("/", function(req, res) {
-    burgerModel.selectAll(function(data) {
-        let burgerObj = {
-            burgers: data
-        };
-        console.log(burgerObj)
-        res.render("index", burgerObj);
-    })
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
 router.post("/api/burgers", function(req, res) {
-    burgerModel.insertOne(req.body.burgerName, function(result) {
-        console.log("Burger posted:" + result);
-        res.status(200).end();
-    
-    })
+  burger.insert(req.body.name, function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-    let id =req.params.id; +id;
-    let condition = 'id = ${id}';
+  let id = "id = " + req.params.id;
 
-    burgerModel.updateOne(
-        condition, function(result) {
-            if(result.changedRows === 0) {
-                res.status(404).end();
-            }
-            res.status(200).end();
-
-        })
+  console.log("id", id);
+  burger.update(
+    req.body.devoured, id, function(res) {
+    // if (res.changedRows === 0) {
+    //   return res.status(404).end();
+    // } 
+    // else {
+    //   res.status(200).end();
+    // }
+  });
 });
+
+module.exports = router;
+
+
+// const express = require("express");
+// const router = express.Router();
+
+
+// const burgerModel = require("../models/burger");
+
+// router.get("/", function(req, res) {
+//     burgerModel.selectAll(function(data) {
+//         let burgerObj = {
+//             burgers: data
+//         };
+//         console.log(burgerObj)
+//         res.render("index", burgerObj);
+//     })
+// });
+
+// router.post("/api/burgers", function(req, res) {
+//     burgerModel.insertOne(req.body.burgerName, function(result) {
+//         console.log("Burger posted:" + result);
+//         res.status(200).end();
+    
+//     })
+// });
+
+// router.put("/api/burgers/:id", function(req, res) {
+//     let id =req.params.id; +id;
+//     let condition = 'id = ${id}';
+
+//     burgerModel.updateOne(
+//         condition, function(result) {
+//             if(result.changedRows === 0) {
+//                 res.status(404).end();
+//             }
+//             res.status(200).end();
+
+//         })
+// });
