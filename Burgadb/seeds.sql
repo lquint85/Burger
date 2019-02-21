3f7b78dd36c3a7727149018396b3a60cf62450c2
Lucent <lquint85@gmail.com>
2019-02-21 16:15:06 -0600

    modified seeds

--- Burgadb/seeds.sql
+++ Burgadb/seeds.sql
@@ -1,12 +1,12 @@
-INSERT INTO burgers (burger_name, eaten)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ('BIG MAC', false);
 
-INSERT INTO burgers (burger_name, eaten)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ('Whopper', false);
 
-INSERT INTO burgers (burger_name, eaten)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ('Heines', false);
 
-INSERT INTO burgers (burger_name, eaten)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ('Heines with Hot Sauce', false);
 
