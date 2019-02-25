dc51483013cd3afcaba2f99bfcae6d3f3e398233
Lucent <lquint85@gmail.com>
2019-02-24 21:43:47 -0600

    modified schema

--- Burgadb/seeds.sql
+++ Burgadb/seeds.sql
@@ -2,11 +2,11 @@ INSERT INTO burgers (burger_name, devoured)
 VALUES ('BIG MAC', false);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ('Whopper', 2;
+VALUES ('Whopper', 2);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ('Heines', false);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ('Heines with Hot Sauce', 2;
+VALUES ('Heines with Hot Sauce', 2);
 
